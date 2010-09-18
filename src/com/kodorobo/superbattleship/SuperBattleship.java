2c01e984473b14b4eb0d2812d5d00aab9f986363
Daniel MacDougall <dmac@ooyala.com>
2010-09-17 22:59:34 -0700

    Set up project and display an ocean

---
+++ src/com/kodorobo/superbattleship/SuperBattleship.java
@@ -0,0 +1,13 @@
+package com.kodorobo.superbattleship;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SuperBattleship extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
