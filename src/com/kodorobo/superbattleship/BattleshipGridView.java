484b99c3297222f7b7d1664ab5bb6c73d29f5e3e
Daniel MacDougall <dmac@ooyala.com>
2010-10-03 02:41:23 -0700

    Implement Ship class, plus simple ship drag-and-drop.

--- src/com/kodorobo/superbattleship/BattleshipGridView.java
+++ src/com/kodorobo/superbattleship/BattleshipGridView.java
@@ -7,33 +7,85 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Paint.Style;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class BattleshipGridView extends View {
 
+	private final String TAG = "Battleship";
+
+	public static final int SHIP_START_Y = 330;
+	public static final int CRUISER_START_X = 30;
+
+	private float cellSize;
 	private final int OUTER_PADDING = 5;
-	private final Paint paint;
-	private final Rect border;
+	private Paint paint;
+	private Rect border;
+	private final Context context;
+
+	private Ship ships[] = new Ship[1];
+	private Ship cruiser;
+	private Ship draggingShip = null;
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		switch(event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			Log.d(TAG, "touch event down");
+			for (int i = 0; i < ships.length; i++) {
+				if (ships[i].containsPoint((int)event.getX(), (int)event.getY())) {
+					draggingShip = ships[i];
+					break;
+				}
+			}
+			break;
+		case MotionEvent.ACTION_MOVE:
+			Log.d(TAG, "touch event move");
+			if (draggingShip != null) {
+				draggingShip.setX((int)(event.getX() - draggingShip.getWidth() / 2));
+				draggingShip.setY((int)(event.getY() - draggingShip.getHeight() / 2));
+			}
+			invalidate();
+			break;
+		case MotionEvent.ACTION_UP:
+			Log.d(TAG, "touch event up");
+			draggingShip = null;
+			break;
+		}
+		return true;
+	}
 
 	public BattleshipGridView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		this.context = context;
 		paint = new Paint();
 		paint.setColor(Color.WHITE);
 		paint.setStyle(Style.STROKE);
-		border = new Rect(OUTER_PADDING, OUTER_PADDING, 0, 0);
 	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		this.border = new Rect(OUTER_PADDING, OUTER_PADDING, w - OUTER_PADDING, w - OUTER_PADDING);
+		this.cellSize = (float) (border.width() / 10.0);
+		resetShips();
+		super.onSizeChanged(w, h, oldw, oldh);
+	};
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		border.set(border.left, border.top,
-				canvas.getWidth() - OUTER_PADDING, canvas.getWidth() - OUTER_PADDING);
 		canvas.drawRect(border, paint);
 		drawGridLines(canvas);
+		cruiser.draw(canvas);
+	}
+
+	private void resetShips() {
+		cruiser = new Ship(context, Ship.ShipType.CRUISER, cellSize);
+		ships[0] = cruiser;
 	}
 
 	private void drawGridLines(Canvas canvas) {
-		float cellSize = border.width()/10;
 		for (int col = 1; col < 10; col++) {
 			canvas.drawLine(OUTER_PADDING + cellSize * col, OUTER_PADDING,
 					OUTER_PADDING + cellSize * col, OUTER_PADDING + border.height(), paint);
@@ -43,5 +95,4 @@ public class BattleshipGridView extends View {
 					OUTER_PADDING + border.height(), OUTER_PADDING + cellSize * row, paint);
 		}
 	}
-
 }
---
+++ src/com/kodorobo/superbattleship/Ship.java
@@ -0,0 +1,120 @@
+package com.kodorobo.superbattleship;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+
+public class Ship {
+
+	public enum ShipType {
+		CARRIER, BATTLESHIP, CRUISER, SUBMARINE, DESTROYER
+	}
+
+	public enum Orientation {
+		HORIZONTAL, VERTICAL
+	}
+
+	private int x;
+	private int y;
+	private int size;
+	private ShipType type;
+	private Orientation orientation;
+	private BitmapDrawable bitmap_horizontal;
+	private BitmapDrawable bitmap_vertical;
+	private float cellSize;
+	private Paint paint;
+	private Context context;
+
+	public Ship(Context context, ShipType type, float cellSize) {
+		this.context = context;
+		this.type = type;
+		this.cellSize = cellSize;
+		this.paint = new Paint();
+		switch (type) {
+			case CARRIER: size = 5; break;
+			case BATTLESHIP: size = 4; break;
+			case CRUISER: size = 3; break;
+			case SUBMARINE: size = 3; break;
+			case DESTROYER: size = 2; break;
+		}
+		this.x = BattleshipGridView.CRUISER_START_X;
+		this.y = BattleshipGridView.SHIP_START_Y;
+		this.orientation = Orientation.VERTICAL;
+		this.bitmap_horizontal = getBitmapForShip(type);
+		this.bitmap_horizontal = scaleBitmapToGrid(bitmap_horizontal);
+		this.bitmap_vertical = rotateBitmap(bitmap_horizontal);
+	}
+
+	public void draw(Canvas canvas) {
+		BitmapDrawable bitmap = (BitmapDrawable) valueByOrientation(bitmap_horizontal, bitmap_vertical);
+		canvas.drawBitmap(bitmap.getBitmap(), x, y, paint);
+	}
+
+	public boolean containsPoint(int x, int y) {
+		return (x > this.x && x < this.x + getWidth() &&
+			y > this.y && y < this.y + getHeight());
+	}
+
+	public int getX() { return this.x; }
+	public int getY() { return this.y; }
+	public void setX(int x) { this.x = x; }
+	public void setY(int y) { this.y = y; }
+
+	public int getWidth() {
+		return ((BitmapDrawable)valueByOrientation(bitmap_horizontal, bitmap_vertical))
+			.getBitmap().getWidth();
+	}
+
+	public int getHeight() {
+		return ((BitmapDrawable)valueByOrientation(bitmap_horizontal, bitmap_vertical))
+			.getBitmap().getHeight();
+	}
+
+	private BitmapDrawable getBitmapForShip(ShipType type) {
+		switch(type) {
+		case CRUISER:
+		default:
+			return new BitmapDrawable(
+					BitmapFactory.decodeResource(context.getResources(), R.drawable.cruiser));
+		}
+	}
+
+	private BitmapDrawable rotateBitmap(BitmapDrawable bitmap) {
+		Bitmap originalBitmap = bitmap.getBitmap();
+		int width = originalBitmap.getWidth();
+		int height = originalBitmap.getHeight();
+		Matrix m = new Matrix();
+		m.postRotate(90);
+		Bitmap rotatedBitmap = Bitmap.createBitmap(originalBitmap, 0, 0, width, height, m, true);
+		return new BitmapDrawable(rotatedBitmap);
+	}
+
+	// Assumes we're dealing with the original, horizontal image.
+	private BitmapDrawable scaleBitmapToGrid(BitmapDrawable bitmap) {
+		Bitmap originalBitmap = bitmap.getBitmap();
+		int width = originalBitmap.getWidth();
+		int height = originalBitmap.getHeight();
+		float newWidth = cellSize * size;
+		float newHeight = cellSize;
+		Matrix m = new Matrix();
+		m.postScale(newWidth / width, newHeight / height);
+		Bitmap newBitmap = Bitmap.createBitmap(originalBitmap, 0, 0, width, height, m, true);
+		return new BitmapDrawable(newBitmap);
+	}
+
+	private Object valueByOrientation(Object ifHorizontal, Object ifVertical) {
+		return (orientation == Orientation.HORIZONTAL ? ifHorizontal : ifVertical);
+	}
+
+	private int valueByOrientation(int ifHorizontal, int ifVertical) {
+		return (orientation == Orientation.HORIZONTAL ? ifHorizontal : ifVertical);
+	}
+
+	private float valueByOrientation(float ifHorizontal, float ifVertical) {
+		return (orientation == Orientation.HORIZONTAL ? ifHorizontal : ifVertical);
+	}
+}
